5d95e146c548ccdd2c805f024a2d8e527d5ac69a
possani <hugopossani@gmail.com>
2017-09-07 14:18:13 -0300

    Closes #15
    Removal of @PropertySource annotation. Addition of changes in yml to the commit.

--- src/main/java/com.hedwig.morpheus/configuration/ApiConfig.java
+++ src/main/java/com.hedwig.morpheus/configuration/ApiConfig.java
@@ -4,7 +4,6 @@ import com.hedwig.morpheus.converter.MessageDtoToMessageConverter;
 import com.hedwig.morpheus.converter.MqttMessageToMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.ConversionServiceFactoryBean;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.core.convert.converter.Converter;
@@ -17,7 +16,6 @@ import java.util.Set;
  */
 
 @Configuration
-@PropertySource("classpath:/application.yml")
 public class ApiConfig {
 
     public Set<Converter> getConverters() {
